bb452e595b8ff5bb97fc201a7f579079ce374c4e
LeeKyoungtaek <dlrudxor0302@gmail.com>
2023-03-16 10:55:22 +0900

    sql test

---
+++ test.sql
@@ -0,0 +1,11 @@
+use taek;
+show DATABASEs;
+show tables;
+select * from test;
+select name from test;
+select age from test;
+
+create table exam(name varchar(10),hobby varchar(20));
+insert into exam values ('허진','운동하기');
+select * from exam;
+
